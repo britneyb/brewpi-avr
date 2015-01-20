e920424be2a620e748717f0f98de8c984f6eea59
britneyb <mattias.bornvall@gmail.com>
2015-01-20 23:12:40 +0100

    adding MenuButton .cpp and .h

---
+++ brewpi_avr/MenuButton.cpp
@@ -0,0 +1,19 @@
+/* 
+* MenuButton.cpp
+*
+* Created: 2015-01-20 23:08:16
+* Author: björn
+*/
+
+
+#include "MenuButton.h"
+
+// default constructor
+MenuButton::MenuButton()
+{
+} //MenuButton
+
+// default destructor
+MenuButton::~MenuButton()
+{
+} //~MenuButton
---
+++ brewpi_avr/MenuButton.h
@@ -0,0 +1,31 @@
+/* 
+* MenuButton.h
+*
+* Created: 2015-01-20 23:08:17
+* Author: björn
+*/
+
+
+#ifndef __MENUBUTTON_H__
+#define __MENUBUTTON_H__
+
+
+class MenuButton
+{
+//variables
+public:
+protected:
+private:
+
+//functions
+public:
+	MenuButton();
+	~MenuButton();
+protected:
+private:
+	MenuButton( const MenuButton &c );
+	MenuButton& operator=( const MenuButton &c );
+
+}; //MenuButton
+
+#endif //__MENUBUTTON_H__
