f788fe6207c58d56badb53567a32c79c0c55a505
britneyb <mattias.bornvall@gmail.com>
2015-01-20 22:27:13 +0100

    adding bubblecounter class

---
+++ brewpi_avr/bubblecounter.cpp
@@ -0,0 +1,19 @@
+/* 
+* Bubblecounter.cpp
+*
+* Created: 2015-01-20 22:08:20
+* Author: björn
+*/
+
+
+#include "Bubblecounter.h"
+
+// default constructor
+Bubblecounter::Bubblecounter()
+{
+} //Bubblecounter
+
+// default destructor
+Bubblecounter::~Bubblecounter()
+{
+} //~Bubblecounter
---
+++ brewpi_avr/bubblecounter.h
@@ -0,0 +1,31 @@
+/* 
+* Bubblecounter.h
+*
+* Created: 2015-01-20 22:08:20
+* Author: björn
+*/
+
+
+#ifndef __BUBBLECOUNTER_H__
+#define __BUBBLECOUNTER_H__
+
+
+class Bubblecounter
+{
+//variables
+public:
+protected:
+private:
+
+//functions
+public:
+	Bubblecounter();
+	~Bubblecounter();
+protected:
+private:
+	Bubblecounter( const Bubblecounter &c );
+	Bubblecounter& operator=( const Bubblecounter &c );
+
+}; //Bubblecounter
+
+#endif //__BUBBLECOUNTER_H__
